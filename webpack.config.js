de33ba234306b80418fc41b41b2b2ab18ca592e8
Micheal <oguntayomicheal@gmail.com>
2022-10-04 18:21:02 +0100

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[main].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+
+  devServer: {
+    static: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.filename$/,
+        use: ["loader-b", "loader-a"]
+      }
+    ]
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
+
